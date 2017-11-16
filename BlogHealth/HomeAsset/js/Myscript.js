d1ef72385aa2834be5e92d1f102eed6b49a92f14
VanHC <VanHC@DESKTOP-6DK60H1>
2017-11-16 22:23:19 +0700

    fix nava

---
+++ BlogHealth/HomeAsset/js/Myscript.js
@@ -0,0 +1,21 @@
+﻿$(window).scroll(function () {
+    if ($(this).scrollTop() > 400) {
+        $('#nav-fix-top').addClass('fixed-top');
+        //$('#UtiPost').show();
+
+        //$('.fx').fadeIn();
+        //$('.aria-contact').fadeIn();
+    } else {
+        $('#nav-fix-top').removeClass('fixed-top');
+       // $('#UtiPost').hide();
+    }
+    //if ($("#fotterStop").isVisible()) {
+    //    $("#UtiPost").hide();
+    //} else {
+    //    $("#UtiPost").show();
+    //}
+    //if ($('body').height() <= ($(window).height() + $(window).scrollTop()+200)) {
+    //    $('#UtiPost').hide();
+    //}
+
+});
