12acb56e7f165e057dad227b5d9c53e38ff3ef22
Cty <User@W010925388-001>
2017-11-17 13:32:35 +0700

    Config animate for navbar

--- BlogHealth/HomeAsset/js/Myscript.js
+++ BlogHealth/HomeAsset/js/Myscript.js
@@ -1,21 +1,40 @@
-﻿$(window).scroll(function () {
-    if ($(this).scrollTop() > 400) {
-        $('#nav-fix-top').addClass('fixed-top');
-        //$('#UtiPost').show();
-
-        //$('.fx').fadeIn();
-        //$('.aria-contact').fadeIn();
-    } else {
+﻿var lastScrollTop = 0;
+$(window).scroll(function (event) {
+    var st = $(this).scrollTop();
+    if (st > lastScrollTop) {
+        // downscroll code
         $('#nav-fix-top').removeClass('fixed-top');
-       // $('#UtiPost').hide();
+        $('#nav-fix-top').removeClass(' animated fadeInDown');
+    } else {
+        $('#nav-fix-top').addClass('fixed-top');
+        $('#nav-fix-top').addClass('animated fadeInDown');
+        if (st < 100) {
+            $('#nav-fix-top').removeClass('fixed-top');
+            $('#nav-fix-top').removeClass('animated fadeInDown');
+        }
+        // upscroll code
     }
-    //if ($("#fotterStop").isVisible()) {
-    //    $("#UtiPost").hide();
-    //} else {
-    //    $("#UtiPost").show();
-    //}
-    //if ($('body').height() <= ($(window).height() + $(window).scrollTop()+200)) {
-    //    $('#UtiPost').hide();
-    //}
-
+    lastScrollTop = st;
 });
+
+//$(window).scroll(function () {
+//    if ($(this).scrollTop() > 400) {
+//        $('#nav-fix-top').addClass('fixed-top');
+//        //$('#UtiPost').show();
+
+//        //$('.fx').fadeIn();
+//        //$('.aria-contact').fadeIn();
+//    } else {
+//        $('#nav-fix-top').removeClass('fixed-top');
+//       // $('#UtiPost').hide();
+//    }
+//    //if ($("#fotterStop").isVisible()) {
+//    //    $("#UtiPost").hide();
+//    //} else {
+//    //    $("#UtiPost").show();
+//    //}
+//    //if ($('body').height() <= ($(window).height() + $(window).scrollTop()+200)) {
+//    //    $('#UtiPost').hide();
+//    //}
+
+//});
