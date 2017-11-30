5d312dbfebc46367e669fd0ce3749346f32313a8
VanHC <VanHC@DESKTOP-6DK60H1>
2017-11-30 21:04:14 +0700

    create database categories

--- BlogHealth/Controllers/AdminController.cs
+++ BlogHealth/Controllers/AdminController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using BlogHealth.Models;
 namespace BlogHealth.Controllers
 {
     public class AdminController : Controller
@@ -12,6 +12,18 @@ namespace BlogHealth.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Categories()
+        {
+            return View();
+        }
+        public ActionResult GetCategories()
+        {
+            using (var ctx = new BlogHealthEntities())
+            {
+                return Json(ctx.Categories.OrderBy(c=>c.Priority).ToList(), JsonRequestBehavior.AllowGet);
+            }
+
         }
     }
 }
