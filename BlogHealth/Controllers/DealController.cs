a19489ee2012025f49994e2775d84e3f7917b5a4
VanHC <VanHC@DESKTOP-6DK60H1>
2017-12-13 21:26:31 +0700

    deal

---
+++ BlogHealth/Controllers/DealController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogHealth.Controllers
+{
+    public class DealController : Controller
+    {
+        // GET: Deal
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
